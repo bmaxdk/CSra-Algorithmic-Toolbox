9e3986b777e93e4e571c757f720e4e963d33e6dd
bmaxdk <choh8uw@gmail.com>
2021-05-09 09:01:48 -0700

    Update change.cpp file

--- week3_greedy_algorithms/1_money_change/change.cpp
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -2,6 +2,31 @@
 
 int get_change(int m) {
   //write your code here
+  int n = 0;
+  while(m != 0){
+    if(m/10 != 0){
+      int r = m/10;
+      n += r;
+      m -= 10*r;
+      // std::cout << "at 10 m =" << m;
+    }
+    if(m/5 != 0){
+      int r = m/5;
+      n += r;
+      m -= 5*r;
+      //std::cout << "at 5 m =" << m;
+
+    }
+    if(m/1 != 0){
+      int r = m;
+      n += r;
+      m -= r;
+      // std::cout << "at 1 m =" << m;
+
+    }
+  }
+
+
   return n;
 }
 
