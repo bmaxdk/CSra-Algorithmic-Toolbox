6fd912605094c61840e023598630d105cc2e11d8
bmaxdk <choh8uw@gmail.com>
2021-05-10 20:02:24 -0700

    Update dot_product.cpp file

--- week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -6,6 +6,24 @@ using std::vector;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
   // write your code here
+  // ----------------------------------------------
+  // this myclass is to make my algorithm to sort descending order bigger >smaller
+  struct myclass {
+  bool operator() (int i, int j) { return (i>j);}
+} myobject;
+
+
+
+//sort <<<<#here has issu!!!!!
+//   struct greater
+// {
+//     template<class T>
+//     bool operator()(T const &a, T const &b) const { return a > b; }
+// };
+  sort (a.begin(), a.end(), myobject);
+  sort (b.begin(), b.end(), myobject);
+
+
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
